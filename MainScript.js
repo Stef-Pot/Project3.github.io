52be4acbce499dc188e335bee9fde2b025b0211c
Stef-Pot <70586582+Stef-Pot@users.noreply.github.com>
2021-12-08 05:50:07 +0200

    new

---
+++ MainScript.js
@@ -0,0 +1,29 @@
+﻿const peronal_block = document.querySelector('#PersonalB');
+const ed_block = document.querySelector('#EducationB');
+const cer_block = document.querySelector('#CerB');
+const proj_block = document.querySelector('#ProjB');
+const skill_block = document.querySelector('#SkillB');
+const more_block = document.querySelector('#MoreB');
+
+const peronalCB = document.querySelector('#PersonalBox');
+peronalCB.addEventListener('change', function (e) {
+    if (peronalCB.checked) {
+        peronal_block.style.display = "none";
+    }
+    else {
+        peronal_block.style.display = "intial";
+    }
+});
+const edCB = document.querySelector('#EducationBox');
+const cerCB = document.querySelector('#CertificationBox');
+const projCB = document.querySelector('#ProjectBox');
+const skillCB = document.querySelector('#SkillsBox');
+const MoreCB = document.querySelector('#MoreBox');
+
+
+
+
+
+
+
+
